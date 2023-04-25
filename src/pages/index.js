5b3f98562f2d051cafda1544c3d293f96bb020d9
xiao1bai2ge3 <53584284+xiao1bai2ge3@users.noreply.github.com>
2023-04-25 10:27:47 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -192,7 +192,8 @@ export default function Home() {
 
       <div className={styles.container}>
         <header className={styles.header}>
-          <h1 className={styles.h1}>查询 ChatGPTAPI 余额</h1>
+          <h1 className={styles.h1}>ChatGPTAPI</h1>
+          <h1 className={styles.h1}>余额查询</h1>
         </header>
         <main className={styles.main}>
 
